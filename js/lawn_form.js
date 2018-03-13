d4d702fa6370fe4a54b16a0afc41e8b543a92ac8
ryansallee <rcsall11@g.holycross.edu>
2018-03-12 21:50:02 -0400

    Located CORS testing server and placed in jQuery post script.

--- js/lawn_form.js
+++ js/lawn_form.js
@@ -1,15 +1,15 @@
 // This form will use the jQuery Post Function to send data to a
-// free server
+// free server that uses back end CORS to send request so that browsers
+// will allow a cross domain request.
 
-var url = "http://ptsv2.com/t/jhffd-1520552880/post";
+var url = "https://cors-test.appspot.com/test";
 var formData = $('form').serialize();
 
 $('input[type="button"]').click(function(evt) {
     evt.preventDefault();
+
     $.post(url, formData, function(response) {
         var success = "<p> Thank you for contacting us!</p>";
         $('form').html(success);
     })
 });
-
-// Research needed-JSONP so we can do cross domain. Or research something different.

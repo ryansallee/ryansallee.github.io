1307e395dba54d9a48b7248b4a8d78502b296dbb
ryansallee <rcsall11@g.holycross.edu>
2018-03-20 23:23:35 -0400

    Updated the index page, added buttons on our_work with JS functions and styles.

--- js/lawn_script.js
+++ js/lawn_script.js
@@ -3,13 +3,18 @@ slideNumber = 0;
 w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
 var targetWidth = 640;
 
-var slideInterval = setInterval(showSlides, 1000);
+var slideInterval = setInterval(slideShow, 1000);
 
-function showSlides() {
+var playing = true;
+var pauseButton = document.querySelector('#pause');
+var resumeButton = document.querySelector('#resume');
+
+
+function slideShow() {
     var i = 0;
     var slidesMinus = document.querySelectorAll('.slides');
     if (w >= targetWidth) {
-        for (i = 0; i < slides.length; i += 1) {
+        for (i; i < slides.length; i += 1) {
             slidesMinus[i].style.display = "none"
         }
         slideNumber += 1;
@@ -21,6 +26,32 @@ function showSlides() {
     }
 };
 
+function stop() {
+    if (playing) {
+        clearInterval(slideInterval);
+        playing = false;
+    }
+}
+
+function resume() {
+    if (!playing) {
+        slideInterval = setInterval(slideShow, 1000);
+        playing = true;
+    }
+}
+
+pauseButton.onclick = function() {
+    stop();
+}
+
+resumeButton.onclick = function() {
+    resume();
+}
+
+if (w < targetWidth) {
+    document.querySelector('#pause').style.display = "none";
+    document.querySelector('#resume').style.display = "none"
+}
 
 // function showSlides () {
 //     for (i; i < slides.length; i += 1) {

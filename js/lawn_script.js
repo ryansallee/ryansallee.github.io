27ae315fc8a43349c1af75bbb7a1288eec04d1af
ryansallee <rcsall11@g.holycross.edu>
2018-03-21 23:06:02 -0400

    Trying to evaluate slideshow function.

--- js/lawn_script.js
+++ js/lawn_script.js
@@ -1,5 +1,7 @@
-const slides = document.querySelectorAll('.slides');
+var slides = document.querySelectorAll('.slides');
+var slidesMinus = document.querySelectorAll('.slides');
 slideNumber = 0;
+var i;
 w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
 var targetWidth = 640;
 
@@ -9,12 +11,14 @@ var playing = true;
 var pauseButton = document.querySelector('#pause');
 var resumeButton = document.querySelector('#resume');
 var nextButton = document.querySelector('#next');
+var previousButton = document.querySelector('#previous');
+
 
 
 function slideShow() {
-    var i = 0;
-    var slidesMinus = document.querySelectorAll('.slides');
+
     if (w >= targetWidth) {
+        i = 0;
         for (i; i < slides.length; i += 1) {
             slidesMinus[i].style.display = "none"
         }
@@ -23,20 +27,9 @@ function slideShow() {
             slideNumber = 1;
         }
         slides[slideNumber - 1].style.display = "block";
-        nextButton.onclick = function() {
-            clearInterval(slideInterval);
-            slides[slideNumber - 1].style.display = "none";
-            for (i; i < slides.length; i += 1) {
-                slidesMinus[i].style.display = "none"
-            }
-            slideNumber += 1;
-            if (slideNumber > slides.length) {
-                slideNumber = 1;
-            }
-            slides[slideNumber - 1].style.display = "block";
-            slideInterval = setInterval(slideShow, 2000);
-        }
-
+    }
+    nextButton.onclick = function() {
+        next();
     }
 };
 
@@ -81,4 +74,38 @@ if (w < targetWidth) {
 //     }
 //     slides[slideNumber].style.display = "none";
 //     slides[slideNumber + 1].style.display = "block";
+// }
+
+// nextButton.onclick = function() {
+//     clearInterval(slideInterval);
+//     if (slideNumber = 1) {
+//         slides[0].style.display = "none";
+//         slides[1].style.display = "block";
+//     } else {
+//         for (i = 0; i < slides.length; i += 1) {
+//             slidesMinus[i].style.display = "none"
+//         }
+//         slideNumber += 1;
+//         if (slideNumber > slides.length) {
+//             slideNumber = 1;
+//         }
+//         slides[slideNumber - 1].style.display = "block";
+//         slideInterval = setInterval(slideShow, 2000);
+//     }
+// }
+// previousButton.onclick = function() {
+//     clearInterval(slideInterval);
+
+//     slides[slideNumber - 1].style.display = "none";
+//     for (i; i < slides.length; i += 1) {
+//         slidesMinus[i].style.display = "none";
+//     }
+//     if (slideNumber < slides.length) {
+//         slideNumber = 3;
+
+//     }
+//     if (slideNumber > slides.length) {
+//         slideNumber = 1;
+//     }
+//     slides[slideNumber].style.display = "block";
 // }

b6bf0c6217cecf2f64d71711d7d963aa2a0c8eb7
ryansallee <rcsall11@g.holycross.edu>
2018-03-23 18:37:23 -0400

    Fixed slideshow in JS, JS to hide buttons on cell phone on Our_Work page, as well as removed from all pages about us. Added services.html page.

---
+++ js/lawn_slide_show.js
@@ -0,0 +1,85 @@
+var slides = document.querySelectorAll('.slides');
+slideNumber = 0;
+w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+var targetWidth = 640;
+
+var slideShowArrayLength = slides.length - 1
+var slideInterval = setInterval(slideShow, 2000);
+
+var playing = true;
+var pauseButton = document.querySelector('#pause');
+var resumeButton = document.querySelector('#resume');
+var nextButton = document.querySelector('#next');
+var previousButton = document.querySelector('#previous');
+
+function slideMove(n) {
+    slideShow(slideNumber += n);
+}
+
+function slideDisplayed(n) {
+    slideShow(slideNumber = n);
+}
+
+
+function slideShow(n) {
+
+    if (w >= targetWidth) {
+        var i = 0;
+        if (n == undefined) {
+            n = ++slideNumber;
+        }
+        if (n > slideShowArrayLength) {
+            slideNumber = 0;
+        }
+        if (n < 0) {
+            slideNumber = slides.length - 1;
+        }
+        for (i; i < slides.length; i += 1) {
+            slides[i].style.display = "none"
+        }
+
+        slides[slideNumber].style.display = "block";
+    }
+
+};
+
+nextButton.onclick = function() {
+    slideMove(1);
+    clearInterval(slideInterval);
+    slideInterval = setInterval(slideShow, 2000);
+}
+
+previousButton.onclick = function() {
+    slideMove(-1);
+    clearInterval(slideInterval);
+    slideInterval = setInterval(slideShow, 2000);
+}
+
+function stop() {
+    if (playing) {
+        clearInterval(slideInterval);
+        playing = false;
+    }
+}
+
+function resume() {
+    if (!playing) {
+        slideInterval = setInterval(slideShow, 2000);
+        playing = true;
+    }
+}
+
+pauseButton.onclick = function() {
+    stop();
+}
+
+resumeButton.onclick = function() {
+    resume();
+}
+
+if (w < targetWidth) {
+    pauseButton.style.display = "none";
+    resumeButton.style.display = "none";
+    previousButton.style.display = "none";
+    nextButton.style.display = "none";
+}

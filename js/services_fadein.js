c3f6ef300b8314113b1fd1242103f94d7db27089
ryansallee <rcsall11@g.holycross.edu>
2018-03-24 12:54:28 -0400

    Added commentary to the services_fadein.js script.

--- js/services_fadein.js
+++ js/services_fadein.js
@@ -1,17 +1,28 @@
-w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+// This script is used to fade in the images and the list items on the services.html page over 4 seconds
+// to add a dramatic event to the page load. This script will only run on tablet sized viewports and larger
+
+// Set variables to detect the viewport width and to set the minimum width for the fadeInOnLoad function to apply
+var viewPortWith = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
 var targetWidth = 640;
 
-fader();
+// Call the fadeInOnLoad function to execute
+fadeInOnLoad();
 
-function fader() {
-    if (w >= targetWidth) {
-        $('.service-wrap img').css("opacity", "0").animate({
-                "opacity": "1",
-            },
-            2000);
-        $('.service-wrap li').delay(2000).css("opacity", "0").animate({
-                "opacity": "1",
-            },
-            2000);
+function fadeInOnLoad() {
+    // Only execute the code if the detected viewport width is greater than or equal to 640px
+    if (viewPortWith >= targetWidth) {
+        // Set the opacity of the images in the .service-wrap div to 0 on page load as the script is right before the 
+        // closing body tag.
+        $('.service-wrap img').css("opacity", "0")
+            // Gradually increase the opacity of the images to 1 over 2 seconds
+            .animate({ "opacity": "1" }, 2000);
+
+        // Set the opacity of the list items in the .service-wrap div to 0 on page load.
+        $('.service-wrap li').css("opacity", "0")
+            // Wait 2 seconds so that the .service-wrap images have reached full opacity before the next method is employed
+            .delay(2000)
+            // Gradually bring the opacity of the list items to 1 over 2 seconds.
+            .animate({ "opacity": "1" }, 2000);
     }
 };
+// End of script

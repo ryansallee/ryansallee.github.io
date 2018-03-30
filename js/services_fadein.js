d1e86dfbc60f55412b8f482ed94060259a3a602b
ryansallee <rcsall11@g.holycross.edu>
2018-03-30 19:57:51 -0400

    Updated grammar error in services_fadein.js.

--- js/services_fadein.js
+++ js/services_fadein.js
@@ -1,15 +1,15 @@
 // This script is used to fade in the images and the list items on the services.html page over 4 seconds
-// to add a dramatic event to the page load. This script will only run on tablet sized viewports and larger
+// to add a dramatic event to the page load. This script will only run on tablet sized viewports and larger.
 
-// Set variables to detect the viewport width and to set the minimum width for the fadeInOnLoad function to apply
+// Set variables to detect the viewport width and to set the minimum width for the fadeInOnLoad function to apply.
 var viewPortWith = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
 var targetWidth = 640;
 
-// Call the fadeInOnLoad function to execute
+// Call the fadeInOnLoad function to execute.
 fadeInOnLoad();
 
 function fadeInOnLoad() {
-    // Only execute the code if the detected viewport width is greater than or equal to 640px
+    // Only execute the code if the detected viewport width is greater than or equal to 640px.
     if (viewPortWith >= targetWidth) {
         // Set the opacity of the images in the .service-wrap div to 0 on page load as the script is right before the 
         // closing body tag.

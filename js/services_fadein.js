12f5a0e64937be4cd8f9c53884f6846c8b0182f7
ryansallee <rcsall11@g.holycross.edu>
2018-03-30 20:01:36 -0400

    Updataed grammar in services_fadein.js.

--- js/services_fadein.js
+++ js/services_fadein.js
@@ -19,7 +19,7 @@ function fadeInOnLoad() {
 
         // Set the opacity of the list items in the .service-wrap div to 0 on page load.
         $('.service-wrap li').css("opacity", "0")
-            // Wait 2 seconds so that the .service-wrap images have reached full opacity before the next method is employed
+            // Wait 2 seconds so that the .service-wrap images have reached full opacity before the next method is employed.
             .delay(2000)
             // Gradually bring the opacity of the list items to 1 over 2 seconds.
             .animate({ "opacity": "1" }, 2000);
